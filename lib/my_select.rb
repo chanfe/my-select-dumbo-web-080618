38a61d41ef298a4fce4ff3bade0a07d1372b2e76
Felix Chan <felixchan29@gmail.com>
2018-07-21 19:20:31 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -12,4 +12,4 @@ def my_select(collection)
 end
   
 #puts (1..10).select{ |i|  i % 3 == 0 }
-puts my_select([1,2,3,4,5,6,7,8,9,10]) { |i|  i % 3 == 0 }
+#puts my_select([1,2,3,4,5,6,7,8,9,10]) { |i|  i % 3 == 0 }
